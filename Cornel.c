f567a1a37586a66e65db640d77aaf75aad9f32ec
Cornel86 <126684062+Cornel86@users.noreply.github.com>
2023-03-07 11:15:49 +0100

    Add files via upload

--- Cornel.c
+++ Cornel.c
@@ -1,14 +1,30 @@
-/*
- * Cornel.c
- *
- *  Created on: 7 Mar 2023
- *      Author: call3
- */
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include "Carl.h"
-#include "Louise.h"
-#include "Hanna.h"
-#include "Cornel.h"
+
+
+int sum(int array [])
+{
+
+	int sum = 0;
+	for (int i = 0; i < 5; i++)
+	{
+		sum = sum + array[i];
+	}
+
+	printf("The total sum is: %d\n",sum);
+	return 0;
+
+}
+
+
+int main ()
+
+
+{
+
+
+
+
+
+}
