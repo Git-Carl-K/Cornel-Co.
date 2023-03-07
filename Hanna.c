e9eb5020179f7481b416b82bb23e6833117fda43
Hanna <rohdin.hanna@hotmail.com>
2023-03-07 15:57:36 +0100

    fixed infinity loop

--- Hanna.c
+++ Hanna.c
@@ -28,48 +28,52 @@ int compare_big(const void * a, const void * b)
 //(2) size of dice_pool
 void print_pool(int dice_pool[], int size)
 {
-	for (int i = 0; i < size; i++)
+	for (int i = 0; i < size; i++) {
 		printf ("%d ", dice_pool[i]);
+	}
+	printf("\n");
 }
 
-//checks for a collection of the same dice-face
+//checks how many dice with the same dice-face
 //*parameters:
 //(1) dice_pool of int's
 //(2) size of dice_pool
-//(3) the number to check for
+//(3) the dice-face to check for
 //returns the points collected
-int find_num_in_pool(int dice_pool[], int size, int num) {
+int find(int dice_pool[], int size, int dice_face) {
 	int found = 0;
+	int points = 0;
 	for (int i = 0; i < size; i++) {
-		if (dice_pool[i] == num) {
+		if (dice_pool[i] == dice_face) {
 			found++;
 		}
 	}
 	if (found != 0) {
-		return found * num;
+		points = found * dice_face;
+		return points;
 	} else {
 		return 0;
 	}
 }
 
-//checks for a collection of 3, 4 or 5 of the same number
+//checks for a collection of 3, 4 or 5 dice with the same dice-face
 //*parameters:
-//(1) dice_poolay of int's to check
-//(2) the size of the dice_poolay
-//(3) the number of same int's to check for
+//(1) dice_pool of int's
+//(2) size of dice_pool
+//(3) the number of same int's to check for (3, 4 or 5)
 //returns the points collected
-int x_of_a_kind(int dice_pool[], int size, int num) {
+int x_of_a_kind(int dice_pool[], int size, int x_of_a_kind) {
 	qsort(dice_pool, size, sizeof(int), compare_big);
 	int points = 0;
-	if (num != 5) {
+	if (x_of_a_kind != 5) {
 		for (int i = 0; i < size; i++) {
-			if ((find_num_in_pool(dice_pool, size, dice_pool[i]) / dice_pool[i]) == num) {
-				points += (dice_pool[i] * num);
+			if ((find(dice_pool, size, dice_pool[i]) / dice_pool[i]) == x_of_a_kind) {
+				points += (dice_pool[i] * x_of_a_kind);
 				return points;
 			}
 		}
 	} else {
-		if ((find_num_in_pool(dice_pool, size, dice_pool[i]) / dice_pool[i]) == num) {
+		if ((find(dice_pool, size, dice_pool[0]) / dice_pool[0]) == x_of_a_kind) {
 			points = 50;
 			return points;
 		}
@@ -79,12 +83,12 @@ int x_of_a_kind(int dice_pool[], int size, int num) {
 
 //checks for a small straight
 //*parameters:
-//(1) dice_poolay of int's to check
-//(2) the size of the dice_poolay
+//(1) dice_pool of int's
+//(2) size of dice_pool
 //returns the points collected
 int check_small_straigt(int dice_pool[], int size) {
 	qsort(dice_pool, size, sizeof(int), compare_small);
-	int points = 0;
+	int points = 15;
 	if (dice_pool[0] == 1) {
 		for (int i = 0; i < (size-1); i++) {
 			if (dice_pool[i] == dice_pool[i+1] - 1) {
@@ -96,20 +100,17 @@ int check_small_straigt(int dice_pool[], int size) {
 	} else {
 		return 0;
 	}
-	for (int i = 0; i < size; i++) {
-		points += dice_pool[i];
-	}
 	return points;
 }
 
 //checks for a large straight
 //*parameters:
-//(1) dice_poolay of int's to check
-//(2) the size of the dice_poolay
+//(1) dice_pool of int's
+//(2) size of dice_pool
 //returns the points collected
 int check_large_straigt(int dice_pool[], int size) {
 	qsort(dice_pool, size, sizeof(int), compare_big);
-	int points = 0;
+	int points = 20;
 	if (dice_pool[0] == 6) {
 		for (int i = 0; i < (size-1); i++) {
 			if (dice_pool[i] == dice_pool[i+1] + 1) {
@@ -121,30 +122,33 @@ int check_large_straigt(int dice_pool[], int size) {
 	} else {
 		return 0;
 	}
-	for (int i = 0; i < size; i++) {
-		points += dice_pool[i];
-	}
 	return points;
 }
 
 //checks for pairs
 //*parameters:
-//(1) dice_poolay of int's to check
-//(2) the size of the dice_poolay
-//(3) the number of pairs to check for
+//(1) dice_pool of int's
+//(2) size of dice_pool
+//(3) the number of pairs to check for (1 or 2)
 //returns the points collected
-int check_pairs(int dice_pool[], int size, int num) {
+int check_pairs(int dice_pool[], int size, int pair) {
 	qsort(dice_pool, size, sizeof(int), compare_big);
 	int pairs = 0;
 	int points = 0;
-	for (int i = 0; i < size; ) {
-		if ((find_num_in_pool(dice_pool, size, dice_pool[i]) / dice_pool[i]) >= 2) {
+	for (int i = 0; i < size;) {
+		if ((find(dice_pool, size, dice_pool[i]) / dice_pool[i]) > 3) {
+			return 0;
+		}
+		if ((find(dice_pool, size, dice_pool[i]) / dice_pool[i]) < 2) {
+			i++;
+			continue;
+		} else {
 			pairs++;
 			points += (dice_pool[i] * 2);
-			if (pairs == num) {
+			if (pairs == pair) {
 				return points;
 			} else {
-				i += (find_num_in_pool(dice_pool, size, dice_pool[i]) / dice_pool[i]);
+				i += (find(dice_pool, size, dice_pool[i]) / dice_pool[i]);
 				continue;
 			}
 		}
@@ -161,15 +165,15 @@ int check_pairs(int dice_pool[], int size, int num) {
 int check_full_house(int dice_pool[], int size) {
 	qsort(dice_pool, size, sizeof(int), compare_big);
 	int points = 0;
-	if ((find_num_in_pool(dice_pool, size, dice_pool[0]) / dice_pool[0]) == 3) {
-		if ((find_num_in_pool(dice_pool, size, dice_pool[4]) / dice_pool[4]) == 2) {
+	if ((find(dice_pool, size, dice_pool[0]) / dice_pool[0]) == 3) {
+		if ((find(dice_pool, size, dice_pool[4]) / dice_pool[4]) == 2) {
 			points += (dice_pool[0] * 3);
 			points += (dice_pool[4] * 2);
 			return points;
 		}
 	}
-	if ((find_num_in_pool(dice_pool, size, dice_pool[0]) / dice_pool[0]) == 2) {
-		if ((find_num_in_pool(dice_pool, size, dice_pool[4]) / dice_pool[4]) == 3) {
+	if ((find(dice_pool, size, dice_pool[0]) / dice_pool[0]) == 2) {
+		if ((find(dice_pool, size, dice_pool[4]) / dice_pool[4]) == 3) {
 			points += (dice_pool[0] * 2);
 			points += (dice_pool[4] * 3);
 			return points;
