b6ed4d18bafe3be00c64a3d6d68934051f5a2b22
LouiseLarsson3 <124036302+LouiseLarsson3@users.noreply.github.com>
2023-03-08 11:28:13 +0100

    Update Louise.c

--- Louise.c
+++ Louise.c
@@ -7,6 +7,10 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <Louise.h>
+#include <Cornel.h>
+#include <Carl.h>
+#include <Hanna.h>
 
 /*Function that calculates the sum of dicepool*/
 int sum_pool(int *dice_pool, int size) {
