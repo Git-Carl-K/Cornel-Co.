56b6ecba802b406684cb7b9df70565b4deb28e47
Git-Carl-K <126683156+Git-Carl-K@users.noreply.github.com>
2023-03-08 14:18:38 +0100

    Add files via upload

--- Carl.c
+++ Carl.c
@@ -5,7 +5,10 @@
 #include "Louise.h"
 #include "Hanna.h"
 #include "Carl.h"
+#include <string.h>
 
+
+#define size 5
 int roll_dice()
 {
 	int dice = rand() % 6 + 1;
@@ -13,63 +16,93 @@ int roll_dice()
 }
 
 //Roll pool of dice with x number of dice in dice array
-void roll_pool(int number_of_dice, int dice_pool[])
+void roll_pool(int dice_pool[])
 {
 	printf("Rolling pool\n");
-	for (int i = 0; i < number_of_dice; i++)
+	fflush(stdout);
+	for (int i = 0; i < 5; i++)
 	{
-		dice_pool[i] = roll_dice();
-		printf("%d ", dice_pool[i]);
+		if(dice_pool[i] == 0)
+		{
+			dice_pool[i] = roll_dice();
+			printf("%d ", dice_pool[i]);
+			fflush(stdout);
+		}
 	}
 	qsort(dice_pool, 5, sizeof(int), compare_small);
 	printf("\n Sorted: \n");
-	for (int i = 0; i < number_of_dice; i++)
+	fflush(stdout);
+	for (int i = 0; i < 5; i++)
 	{
 		printf("%d ", dice_pool[i]);
+		fflush(stdout);
 	}
 	printf("\n");
+	fflush(stdout);
 }
 
 
 // Saves the value that you want from said dice_pool. INCOMPLETE
-int save_value(int dice_pool[])
+void save_value(int dice_pool[])
 {
-	int merged_array[5];
-
+	static int merged_array[5];
+	int i = 0;
 
 	printf("Which dice would you like to save, separate dices with space,"
 			" 0 to discard\n");
 	fflush(stdout);
 	scanf("%d %d %d %d %d", &merged_array[0], &merged_array[1],&merged_array[2],
-				&merged_array[3], &merged_array[4]);
+			&merged_array[3], &merged_array[4]);
 	printf("Saved dice: \n");
-	for (int i = 0; i < 5; i++)
+	fflush(stdout);
+	for (i = 0; i < 5; i++)
 	{
 		if (merged_array[i] == dice_pool[i])
 		{
 			printf("dice %d: %d ", i + 1, merged_array[i]);
+			fflush(stdout);
+		}
+	}
+	int dice_left_to_throw = 0;
+
+	printf("\n");
+	fflush(stdout);
+	for (int j = 0; j < 5; j++)
+	{
+		if (merged_array[j] == 0)
+		{
+			dice_left_to_throw++;
 		}
 	}
 
-	printf("\n");
+	printf("Dice left to throw: %d\n", dice_left_to_throw);
+	fflush(stdout);
+	printf("Do you wish to roll again? y/n \n");
+	fflush(stdout);
+	char choice[3];
+	scanf("%s", choice);
 
-	return (0);
+	if (strcmp(choice, "y") == 0)
+	{
+		roll_pool(merged_array);
+	}
 }
 
 // Asks user to roll again
-int roll_again()
-{
-	int dice_pool[5];
-	printf("Do you wish to roll again? y/n \n");
-	fflush(stdout);
-	char choice;
-	scanf("%c", &choice);
-	if (choice == 'y')
-	{
-		roll_pool(5, dice_pool);
-	}
-	return (0);
-}
+//int roll_again()
+//{
+//
+//	int dice_pool[5];
+//	printf("Do you wish to roll again? y/n \n");
+//	fflush(stdout);
+//	char choice;
+//	scanf("%c", &choice);
+//	if (choice == 'y')
+//	{
+//		roll_pool(5, dice_pool);
+//	}
+//	return (0);
+//}
 
 
 
--- Carl.h
+++ Carl.h
@@ -1,7 +1,26 @@
+/*
+ * Carl.h
+ *
+ *  Created on: 7 Mar 2023
+ *      Author: call3
+ */
+
+#ifndef CARL_H_
+#define CARL_H_
+
+
 int roll_dice();
-void roll_pool(int number_of_dice, int dice_pool[]);
-int save_value(int dice_pool[]);
+void roll_pool(int dice_pool[]);
+void save_value(int dice_pool[]);
 int compare_int(const void * a, const void * b);
 void score_board();
 int roll_again();
 
+
+
+
+
+
+
+
+#endif /* CARL_H_ */
