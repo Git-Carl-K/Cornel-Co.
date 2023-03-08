2b58821d89ecbea214f1ea009149c564f2131c06
Git-Carl-K <126683156+Git-Carl-K@users.noreply.github.com>
2023-03-08 10:18:24 +0100

    Add files via upload

--- Carl.c
+++ Carl.c
@@ -21,37 +21,86 @@ void roll_pool(int number_of_dice, int dice_pool[])
 		dice_pool[i] = roll_dice();
 		printf("%d ", dice_pool[i]);
 	}
-	qsort(dice_pool, 5, sizeof(int), compare_int);
+	qsort(dice_pool, 5, sizeof(int), compare_small);
 	printf("\n Sorted: \n");
 	for (int i = 0; i < number_of_dice; i++)
 	{
 		printf("%d ", dice_pool[i]);
 	}
-
+	printf("\n");
 }
 
+
 // Saves the value that you want from said dice_pool. INCOMPLETE
-void save_value(int value_to_save, int dice_pool[])
+int save_value(int dice_pool[])
 {
 	int merged_array[5];
+	int choice;
 
+	printf("Which dice would you like to save?\n");
+	fflush(stdout);
+	scanf("%d", &choice);
 	printf("Saved dice: \n");
 	for (int i = 0; i < 5; i++)
 	{
-		if (value_to_save == dice_pool[i])
+		if (choice == dice_pool[i])
 		{
-			merged_array[i] = value_to_save;
+			merged_array[i] = choice;
 			printf("%d ", merged_array[i]);
 
 		}
 
+
+		//	printf("Which dice would you like to save, separate dices with space");
+		//	fflush(stdout);
+		//	scanf("%d %d %d %d %d", &merged_array[0], &merged_array[1],&merged_array[2],
+		//			&merged_array[3], &merged_array[4]);
+
 	}
+		return (0);
 }
 
-//compare function for quick sort
-int compare_int(const void * a, const void * b)
+// Asks user to roll again
+int roll_again()
 {
-	if ( *(int*)a <  *(int*)b ) return -1;
-	if ( *(int*)a == *(int*)b ) return 0;
-	return 1;
+	int dice_pool[5];
+	printf("Do you wish to roll again? y/n \n");
+	fflush(stdout);
+	char choice;
+	scanf("%c", &choice);
+	if (choice == 'y')
+	{
+		roll_pool(5, dice_pool);
+	}
+	return (0);
 }
+
+
+
+
+// Prints score board for Yatzy
+void score_board()
+{
+	printf("\n\n");
+	//	printf ("             ROLL:0             CATEGORY            PLAYER 1   PLAYER 2\n\n");
+	printf ("[   ] ONES                [      ]   [      ]\n");
+	printf ("[   ] TWOS                [      ]   [      ]\n");
+	printf ("[   ] THREES              [      ]   [      ]\n");
+	printf ("[   ] FOURS               [      ]   [      ]\n");
+	printf ("[   ] FIVES               [      ]   [      ]\n");
+	printf ("[   ] SIXES               [      ]   [      ]\n");
+	printf ("[   ] TOTAL               [      ]   [      ]\n");
+	printf ("[   ] BONUS               [      ]   [      ]\n\n");
+	printf ("[   ] THREE OF A KIND     [      ]   [      ]\n");
+	printf ("[   ] FOUR OF A KIND      [      ]   [      ]\n");
+	printf ("[   ] FULL HOUSE          [      ]   [      ]\n");
+	printf ("[   ] SMALL STRAIGHT      [      ]   [      ]\n");
+	printf ("[   ] LARGE STRAIGHT      [      ]   [      ]\n");
+	printf ("[   ] CHANCE              [      ]   [      ]\n");
+	printf ("[   ] YATZY               [      ]   [      ]\n");
+	printf ("[   ] TOTAL               [      ]   [      ]\n");
+}
+
+
+
+
