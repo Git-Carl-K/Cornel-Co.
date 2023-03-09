00b0e630bb0043eb8125f7c31365bef35317b8e7
Cornel86 <126684062+Cornel86@users.noreply.github.com>
2023-03-09 14:09:00 +0100

    Add files via upload

--- Cornel.h
+++ Cornel.h
@@ -23,8 +23,25 @@ typedef struct top_ten{
 
 }top_ten;
 
+typedef struct score_board_column{
+    // Lots of fields (that we don't need)
+    // Lots of fields (that we don't need)
+    // Lots of fields (that we don't need)
+    // Lots of fields (that we don't need)
+    int total_score; //We only use this one
+
+}score_board_column;
+
+
+typedef struct score_board{
+    char name[50];
+    score_board_column scores;
+
+}score_board;
+
 int compare_top_ten (const void *a, const void *b);
 bool save_top_ten (char *filename,  top_ten players[]);
 bool read_top_ten (char *filename,  top_ten fromfile[]);
 void print_top_ten (top_ten players[]);
 void load_highscore(top_ten *players);
+void add_to_highscore(top_ten *players, score_board to_add);
